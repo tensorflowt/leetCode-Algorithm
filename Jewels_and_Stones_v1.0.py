0484a5c4ccf97f334c3399fee3aad20710bc2198
wentao <37217594+tensorflowt@users.noreply.github.com>
2019-04-15 14:52:38 +0800

    Add files via upload

---
+++ Jewels_and_Stones_v1.0.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numJewelsInStones(self,J,S):
+        J = list(J)
+        S = list(S)
+        List_J = []
+        List_S = []
+        result = []
+        for i in range(len(J)):
+            List_J.append(J[i])
+        for j in range(len(S)):
+            if S[j] not in List_J:
+                result.append(S[j])
+            else:
+                List_S.append(S[j])
+        return len(List_S)
+
+a = Solution()
+b = a.numJewelsInStones("aA","aAAbbbb")
+print(b)
+
+
+
